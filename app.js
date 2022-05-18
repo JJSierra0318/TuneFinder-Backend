2e8c0f652b197f36303206bbf90c917c8e58c573
JJSierra0318 <juanjosesierram@gmail.com>
2022-05-18 18:18:01 -0500

    feat: Connected to MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
